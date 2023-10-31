9820f093a08d8211b715fe1974ebe7dd410d4e18
ahow6552 <a0961500945@gmail.com>
2023-10-31 17:44:05 +0800

    first update

---
+++ Assets/C# Scirpt/CheckerCon.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckerCon : MonoBehaviour
+{
+    private int size = 4;
+    int stage = 0;
+    bool Finish = false;
+    public int Score = 0;
+    // Start is called before the first frame update
+    public Vector3 InitPos;
+    bool Kill = false;
+
+    void Update()
+    {
+    // player finish the game
+        if (stage == size)                                  
+        {
+            Finish = true;
+            AddScore();
+            GameObject.Find("SpawnerManager").GetComponent<SpawnerManager>().Regene();
+            Finish = false;
+            this.transform.position = InitPos;
+            stage = 0;
+            Kill = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Score);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "cubes")
+        {
+            other.GetComponent<item>().SetOn();
+        }
+        if (Kill == true)
+        {
+            Destroy(other.gameObject);
+            this.transform.Translate(2.0f, 0f, 0f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "cubes")
+        {
+            stage++;
+        }
+    }
+
+    public void AddScore()
+    {
+        if (Finish)
+        {
+            Score += 100;
+        }
+    }
+
+    public void Kill_All()
+    {
+        this.transform.Translate(2.0f, 0f, 0f);
+        Kill = true;
+        Score -= 100;
+    }
+}
